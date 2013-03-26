a5c90035f44d09a0a416f879097662659482c1e4
Mike Cendana <mike@headhippo.com>
2013-03-25 23:50:47 -0500

    first attempt to display background image for main menu

--- main.lua
+++ main.lua
@@ -1,48 +1,28 @@
-----------------------------------------------------------------
--- Copyright (c) 2010-2011 Zipline Games, Inc. 
--- All Rights Reserved. 
--- http://getmoai.com
-----------------------------------------------------------------
+screenWidth = MOAIEnvironment.screenWidth
+screenHeight = MOAIEnvironment.screenHeight
 
-MOAISim.openWindow ( "test", 320, 480 )
+-- if host does not provide screen dimensions, default to iphone retina display
+if screenWidth == nil then screenWidth = 960 end
+if screenHeight == nil then screenHeight = 640 end
 
-viewport = MOAIViewport.new ()
-viewport:setSize ( 320, 480 )
-viewport:setScale ( 320, 480 )
+-- required for non mobile platforms
+MOAISim.openWindow("W.O.B. Prototype", screenWidth, screenHeight)
 
-layer = MOAILayer2D.new ()
-layer:setViewport ( viewport )
-MOAISim.pushRenderPass ( layer )
+viewport = MOAIViewport.new()
+-- viewport size will match the device resolution
+viewport:setSize(screenWidth, screenHeight)
+-- viewport will show 960 x 640 world units regardless of device resolution
+viewport:setScale(960, 640)
 
-gfxQuad = MOAIGfxQuad2D.new ()
-gfxQuad:setTexture ( "moai.png" )
-gfxQuad:setRect ( -64, -64, 64, 64 )
+mainMenuLayer = MOAILayer2D.new()
+mainMenuLayer:setViewport(viewport)
+MOAISim.pushRenderPass(mainMenuLayer)
 
-prop = MOAIProp2D.new ()
-prop:setDeck ( gfxQuad )
-prop:setLoc ( 0, 80 )
-layer:insertProp ( prop )
+backgroundQuad = MOAIGfxQuad2D.new()
+backgroundQuad:setTexture("EmptyBackground.png")
+backgroundQuad:setRect(-160, -240, 160, 240)
 
-font = MOAIFont.new ()
-font:loadFromTTF ( "arialbd.ttf", " abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789,.?!", 12, 163 )
+backgroundProp = MOAIProp2D.new()
+backgroundProp:setDeck(backgroundQuad)
 
-textbox = MOAITextBox.new ()
-textbox:setFont ( font )
-textbox:setRect ( -160, -80, 160, 80 )
-textbox:setLoc ( 0, -100 )
-textbox:setYFlip ( true )
-textbox:setAlignment ( MOAITextBox.CENTER_JUSTIFY )
-layer:insertProp ( textbox )
-
-textbox:setString ( "Moai has installed correctly! Check out the samples folder.\n<c:0F0>Meow.<c>" )
-textbox:spool ()
-
-function twirlingTowardsFreedom ()
-	while true do
-		MOAIThread.blockOnAction ( prop:moveRot ( 360, 1.5 ))
-		MOAIThread.blockOnAction ( prop:moveRot ( -360, 1.5 ))
-	end
-end
-
-thread = MOAIThread.new ()
-thread:run ( twirlingTowardsFreedom )
+mainMenuLayer:insertProp(backgroundProp)
